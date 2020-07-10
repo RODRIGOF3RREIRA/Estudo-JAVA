bd999f076f665625d6d4d1141a1079431fc5e69c
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-07-09 23:42:14 -0400

    POO 01

---
+++ Estudo_POO/src/estudo_poo/Cadeira.java
@@ -0,0 +1,24 @@
+
+package estudo_poo;
+
+public class Cadeira {
+    float altura;
+    float altura_assento;
+    String cor;
+    
+    void serrar (){
+        System.out.println("Serrando a madeira...");
+    }
+    
+    void lixar (){
+        System.out.println("Lixando a cadeira");
+    }
+    
+    void pintar (){
+        System.out.println("Pintando a madeira");
+    }
+    
+ 
+        
+    
+}
---
+++ Estudo_POO/src/estudo_poo/Principal.java
@@ -0,0 +1,14 @@
+package estudo_poo;
+
+public class Principal {
+
+  
+    public static void main(String[] args) {
+      Cadeira c1 = new Cadeira();
+      c1.serrar();
+      c1.lixar();
+      c1.pintar();
+        
+    }
+    
+}
